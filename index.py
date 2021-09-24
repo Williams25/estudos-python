1dcdf7893d9f9d8b2a1690a16d3b343b557217ee
William Gabriel <43673479+Williams25@users.noreply.github.com>
2021-09-24 19:47:52 -0300

    Update index.py
    criando codigo para dowload de videos do youtube

--- index.py
+++ index.py
@@ -1,2 +1,12 @@
 messagePython = "curso de python"
 print(messagePython)
+
+from pytube import YouTube
+
+def downloadVideo (link: str) -> str:
+  video = YouTube(link)
+  stream = video.streams.get_highest_resolution()
+  stream.download()
+  return "ok"
+
+print(downloadVideo("https://www.youtube.com/watch?v=SoCp5k2yZ1U"))
