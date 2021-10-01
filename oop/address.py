1a3598c121b14a3953fe327c75af9f8eee3a813b
Williams25 <william.souza.timao@hotmail.com>
2021-10-01 15:15:06 -0300

    estudando sobre orientação a objetos e heranças

---
+++ oop/address.py
@@ -0,0 +1,6 @@
+class Address:
+  def __init__(self, address, city, state, country):
+    self.address = address
+    self.city = city
+    self.state = state
+    self.country = country
---
+++ oop/index.py
@@ -0,0 +1,7 @@
+from people import People
+
+p1 = People("william", 22, True, "gildo nicolucci 650", "matão", "sp", "vila maria")
+p2 = People("taty", 21, True, "teste 650", "matão", "sp", "vila maria")
+
+print(p1.detailsPeople())
+print(p2.detailsPeople())
