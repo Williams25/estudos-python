0c2a72992a16a4b4d616729793ceee6974672378
Williams25 <william.souza.timao@hotmail.com>
2021-10-22 16:48:17 -0300

    itrodução a oop

--- oop/people.py
+++ oop/people.py
@@ -1,21 +1,23 @@
 from address import Address
 
+
 class People(Address):
-  def __init__(self, name, age, active, address, city, state, country):
-    super().__init__(address, city, state, country)
-    self.name = name
-    self.age = age
-    self.active = active
-  
-  def detailsPeople(self):
-    return {
-      'name': self.name,
-      'age': self.age,
-      'active': self.active,
-      'address': {
-        'address': self.address,
-        'city': self.city,
-        'state': self.state, 
-        'country': self.country,
-      },
-    }
+    def __init__(self, name, age, active, address, city, state, country):
+        super().__init__(address, city, state, country)
+        self.name = name
+        self.age = age
+        self.active = active
+
+    def detailsPeople(self):
+
+        return {
+            'name': self.name,
+            'age': self.age,
+            'active': self.active,
+            'address': {
+                'address': self.address,
+                'city': self.city,
+                'state': self.state,
+                'country': self.country,
+            },
+        }
