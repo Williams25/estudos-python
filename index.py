4064ceff5d36ac41472a054c0aba5eafee2da60a
Williams25 <william.souza.timao@hotmail.com>
2021-10-22 16:48:59 -0300

    realizando download de video do youtube em formato mp4 e mp3

--- index.py
+++ index.py
@@ -1,12 +1,37 @@
-messagePython = "curso de python"
-print(messagePython)
-
 from pytube import YouTube
+import os
 
-def downloadVideo (link: str) -> str:
-  video = YouTube(link)
-  stream = video.streams.get_highest_resolution()
-  stream.download()
-  return "ok"
 
-print(downloadVideo("https://www.youtube.com/watch?v=SoCp5k2yZ1U"))
+def downloadVideo(link: str, path: str) -> str:
+    video = YouTube(link)
+
+    if not os.path.exists(path):
+        os.makedirs(path)
+
+    stream = video.streams.get_highest_resolution()
+
+    stream.download(path)
+    return "ok"
+
+
+def downloadMp3(link: str, path: str) -> str:
+    video = YouTube(link)
+
+    if not os.path.exists(path):
+        os.makedirs(path)
+
+    stream = video.streams.filter(
+        only_audio=True).first()
+
+    out_file = stream.download(path)
+
+    base, ext = os.path.splitext(out_file)
+    new_file = base + ".mp3"
+    os.rename(out_file, new_file)
+
+    return "ok"
+
+
+print(downloadVideo(
+    "https://www.youtube.com/watch?v=1Xq77Z9YRko",
+    "../../Área de Trabalho/nlw/aulas/expanse"))
