ae7e1b8b9dd4c58f093e1977f235ae656ec32d76
Williams25 <william.souza.timao@hotmail.com>
2021-11-18 11:04:37 -0300

    Dentro do pacote utilidadesCeV que criamos, temos um módulo chamado dado. Crie uma função chamada leiaDinheiro() que seja capaz de funcionar como a função imputa(), mas com uma validação de dados para aceitar apenas valores que seja monetários.

--- modulos/utilidadesCeV/__init__.py
+++ modulos/utilidadesCeV/__init__.py
@@ -1,3 +1,11 @@
 import dado
+from moeda_calculo import dobro, aumentar, diminuir, metade, formatar_moeda
 
-dado.start()
+valor = dado.leiaDinheiro('Digite o preço R$ ')
+
+print(
+    f"o aumento de preço 10% foi de {formatar_moeda(valor)} para {aumentar(valor, 10, True)}")
+print(
+    f"o preço de {formatar_moeda(valor)} abaixou para 10% {diminuir(valor, 10, True)}")
+print(f"o preço de {formatar_moeda(valor)} dobrou para {dobro(valor, True)}")
+print(f"a medade preço de {formatar_moeda(valor)} é {metade(valor, True)}")
--- modulos/utilidadesCeV/dado/__init__.py
+++ modulos/utilidadesCeV/dado/__init__.py
@@ -1,10 +1,10 @@
-from moeda_calculo import dobro, aumentar, diminuir, metade
+def leiaDinheiro(msg):
+    valido = False
+    while not valido:
+        valor = str(input(msg))
 
-
-def start():
-    print(
-        f"o aumento de preço 10% foi de R$ {'400'} para {aumentar(423, 10, True)}")
-    print(
-        f"o preço de R$ {'423'} abaixou para 10% {diminuir(423, 10, True)}")
-    print(f"o preço de R$ {'423'} dobrou para {dobro(423, True)}")
-    print(f"a medade preço de R$ {'423'} é {metade(423, True)}")
+        if valor.isalpha() or len(valor.strip()) == 0:
+            print(f"\033[0;31mDigite um valor valido!\033[m")
+        else:
+            valido = True
+            return float(valor.replace(",", "."))
