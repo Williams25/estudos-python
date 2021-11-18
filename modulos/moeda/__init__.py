4020534f7c4cdea8208cadfa1c41f949cef1a7bd
Williams25 <william.souza.timao@hotmail.com>
2021-11-18 09:32:38 -0300

    formatando moedas passando parametro formatar como true

--- modulos/moeda/__init__.py
+++ modulos/moeda/__init__.py
@@ -1,7 +1,8 @@
 import calculo
 
 print(
-    f"o aumento de preço 10% foi de R$ {'400'} para {calculo.aumentar(423, 10)}")
-print(f"o preço de R$ {'423'} abaixou para 10% {calculo.diminuir(423, 10)}")
-print(f"o preço de R$ {'423'} dobrou para {calculo.dobro(423)}")
-print(f"a medade preço de R$ {'423'} é {calculo.metade(423)}")
+    f"o aumento de preço 10% foi de R$ {'400'} para {calculo.aumentar(423, 10, True)}")
+print(
+    f"o preço de R$ {'423'} abaixou para 10% {calculo.diminuir(423, 10, True)}")
+print(f"o preço de R$ {'423'} dobrou para {calculo.dobro(423, True)}")
+print(f"a medade preço de R$ {'423'} é {calculo.metade(423, True)}")
