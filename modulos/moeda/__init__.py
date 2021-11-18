396d45f813099706c7fecb61c210f2561e2e4ded
Williams25 <william.souza.timao@hotmail.com>
2021-11-18 09:15:33 -0300

    Crie um módulo chamado moeda que tenha as funções incorporadas aumentar(), diminuir(), dobro() e metade(). Faça também um programa que importe esse módulo e use algumas dessas funções.

---
+++ modulos/moeda/__init__.py
@@ -0,0 +1,6 @@
+import calculo
+
+print(f"o aumento de preço foi de R$ {'400'} para R$ {calculo.aumentar(423)}")
+print(f"o preço de R$ {'423'} abaixou para R$ {calculo.diminuir(423)}")
+print(f"o preço de R$ {'423'} dobrou para R$ {calculo.dobro(423)}")
+print(f"a medade preço de R$ {'423'} é R$ {calculo.metade(423)}")
