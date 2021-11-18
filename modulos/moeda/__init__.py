ba895cdf3ec4ae50e8d633c2d5ff10677f6d2bb4
Williams25 <william.souza.timao@hotmail.com>
2021-11-18 09:26:42 -0300

    testando funções de moeda

--- modulos/moeda/__init__.py
+++ modulos/moeda/__init__.py
@@ -1,6 +1,7 @@
 import calculo
 
-print(f"o aumento de preço foi de R$ {'400'} para R$ {calculo.aumentar(423)}")
-print(f"o preço de R$ {'423'} abaixou para R$ {calculo.diminuir(423)}")
-print(f"o preço de R$ {'423'} dobrou para R$ {calculo.dobro(423)}")
-print(f"a medade preço de R$ {'423'} é R$ {calculo.metade(423)}")
+print(
+    f"o aumento de preço 10% foi de R$ {'400'} para {calculo.aumentar(423, 10)}")
+print(f"o preço de R$ {'423'} abaixou para 10% {calculo.diminuir(423, 10)}")
+print(f"o preço de R$ {'423'} dobrou para {calculo.dobro(423)}")
+print(f"a medade preço de R$ {'423'} é {calculo.metade(423)}")
